72f0150ff343ef97374de19ccf2688093c7bdfbf
acsf <a.carlossf@hotmail.com>
2022-04-19 20:11:25 -0300

    Aula 3 - Layout usuarios

---
+++ Financas/Controllers/UsuarioController.cs
@@ -0,0 +1,41 @@
+﻿using Financas.DAO;
+using Financas.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private UsuarioDAO usuarioDAO;
+
+        public UsuarioController(UsuarioDAO usuarioCtor)
+        {
+            this.usuarioDAO = usuarioCtor;
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+        public ActionResult Adiciona(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuarioDAO.Adiciona(usuario);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Form", usuario);
+            }
+        }
+        public ActionResult Index()
+        {
+            return View(usuarioDAO.Lista());
+        }
+    }
+}
+
